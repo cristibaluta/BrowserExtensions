172f7557bd8120cdbc42b17ca5aecc1087529928
Cristian Baluta <cristian.baluta@fortech.ro>
2017-03-20 14:23:49 +0200

    Make request in background because from content.js is not working

---
+++ Run Jenkins/Chrome/Run Jenkins/background.js
@@ -0,0 +1,26 @@
+/**
+ * Possible parameters for request:
+ *  action: "xhttp" for a cross-origin HTTP request
+ *  method: Default "GET"
+ *  url   : required, but not validated
+ *  data  : data to send in a POST request
+ *
+ * The callback function is called upon completion of the request */
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+    if (request.action == "xhttp") {
+        var xhttp = new XMLHttpRequest();
+        var method = request.method ? request.method.toUpperCase() : 'GET';
+
+        xhttp.onload = function() {
+            callback(xhttp.responseText);
+        };
+        xhttp.onerror = function() {
+            // Do whatever you want on error. Don't forget to invoke the
+            // callback to clean up the communication port.
+            callback();
+        };
+        xhttp.open(method, request.url + "?" + request.data, true);
+        xhttp.send(null);
+        return true; // prevents the callback from being called too early on return
+    }
+});
--- Run Jenkins/Chrome/Run Jenkins/content.js
+++ Run Jenkins/Chrome/Run Jenkins/content.js
@@ -19,33 +19,27 @@ if (matches > 0) {
 	document.body.appendChild ( wrapper );
 
 	wrapper.addEventListener("click", function() {
-	
+		
 		var branchNameDiv = document.getElementsByClassName("branch-name");
 		var branchName = branchNameDiv[0].innerHTML;
 		var bitbucketUrl = window.location.href;
-	
+		
 		chrome.storage.sync.get(['url', 'job', 'token'], function(items) {
-		
-			console.log(items);
-		
+			
 		    var url = items["url"];
 		    var job = items["job"];
 		    var token = items["token"];
 			var params = "job="+job+"&token="+token+"&bitbucket_url="+bitbucketUrl+"&branch_name="+branchName;
-		
-			var http = new XMLHttpRequest();
-			http.open("GET", url+"?"+params, true);
-			http.onreadystatechange = function() {
-			    if (http.readyState == 4) {
-					if (http.status == 404) {
-						alert("Can't call this url "+url+"?"+params);
-					} else if (http.status == 201) {
-						alert("Jenkins will start testing this branch: "+branchName);
-					}
-			    }
-			}
-			http.send(null);
+			
+			chrome.runtime.sendMessage({
+			    method: 'GET',
+			    action: 'xhttp',
+			    url: url,
+			    data: params
+			}, function(responseText) {
+			    alert(responseText);
+			});
 	    });
-	
+		
 	}, false);
 }
